d21e1073458ae28e97d9d0274f99b3383140e472
Gerardo Ospina <ngospina@gmail.com>
2015-04-13 00:44:33 -0500

    Release 14.0.0alpha1

---
+++ Runtime/Sharedlib/sisalrt.h
@@ -0,0 +1,29 @@
+#ifndef SISALRT_H
+#define SISALRT_H
+
+/**************************************************************************/
+/* FILE   **************         sisalrt.h         ************************/
+/**************************************************************************/
+/* Author: Dave Cann                                                      */
+/* Update: Patrick Miller -- Ansi support (Dec 2000)                      */
+/* Copyright (C) University of California Regents                         */
+/**************************************************************************/
+/*
+ * $Log:
+ */
+/**************************************************************************/
+
+#include "sisalInfo.h"
+
+extern void ShutDownDsa PROTO((void));
+extern void ParseTracerCommand PROTO((char*));
+extern void InitDsa PROTO((int,int));
+extern void InitSignalSystem PROTO((void));
+
+#include "locks.h"
+#include "process.h"
+#include "fibre.h"
+#include "arrays.h"
+#include "interface.h"
+
+#endif
